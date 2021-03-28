3c54a670c9d899745a455410006892acb2cdda40
Boomturtle <krystof.lenz@post.cz>
2021-03-29 01:20:34 +0200

    JumpingJumpyUnity
    Udělal jsem základní pohyb (Rychlost se dá změnit v unity když si klikneš na player). Naporgramoval jsem tam i gravitaci kterou můžeme změnit a i skákání(Když si otevřeš ten script tak tam je zakomentovaný kód a to je kód pro double jump kdyby jsme ho tam chtěli)

---
+++ JumpingJumpy/Assets/Scripts/Player.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 5f; //Rychlost pohybu
+    [SerializeField]
+    private float gravity = 9.81f; //Gravitace
+    [SerializeField]
+    private float jumpSpeed = 3.5f; //Výška skoku
+   /* [SerializeField]
+    private float doubleJumpMultiplier = 0.5f;*/ //Double Jump
+
+    private CharacterController controller;
+
+    private float directionY;
+
+   // private bool canDoubleJump = false; //Double Jump
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");//Ovládání pohybu
+        float verticalInput = Input.GetAxis("Vertical");//Ovládání pohybu
+
+        Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);//Smìr pohybu
+
+        if (controller.isGrounded) //Zajištìní aby hráè mohl skákat pouze když je na zemi
+        {
+
+            if (Input.GetButtonDown("Jump")) //Skákání
+            {
+               // canDoubleJump = true; //Double Jump
+                directionY = jumpSpeed;
+            }
+        }
+       /* else //Double Jump
+        {
+            if (Input.GetButtonDown("Jump") && canDoubleJump==true)
+            {
+                directionY = jumpSpeed * doubleJumpMultiplier;
+                canDoubleJump = false;
+            }
+        }*/ 
+
+        directionY-= gravity * Time.deltaTime;//Gravitace
+
+        direction.y = directionY;
+
+        controller.Move(direction * moveSpeed * Time.deltaTime); //Pohyb 
+    }
+}
